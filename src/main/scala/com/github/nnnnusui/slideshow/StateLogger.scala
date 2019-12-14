426a3b70a7956355fd4695f84bc322a697a89ce9
nnnnusui <nnnnusui@gmail.com>
2019-12-15 08:27:00 +0900

    ♻ removed StateLogger's debug print.

--- src/main/scala/com/github/nnnnusui/slideshow/StateLogger.scala
+++ src/main/scala/com/github/nnnnusui/slideshow/StateLogger.scala
@@ -6,19 +6,16 @@ class StateLogger[T](value: T){
   def logging(value: T): Unit ={
     val log = timelineLog.drop(logAccessIndex)
     timelineLog = value :: log.take(9)
-    println(s"$timelineLog")
     logAccessIndex = 0
   }
   def undo(): T ={
     if(logAccessIndex +1 < timelineLog.size)
       logAccessIndex += 1
-    println(s"$logAccessIndex $timelineLog")
     timelineLog(logAccessIndex)
   }
   def redo(): T = {
     if(logAccessIndex > 0)
       logAccessIndex -= 1
-    println(s"$logAccessIndex $timelineLog")
     timelineLog(logAccessIndex)
   }
 }
