df8855d952e5641086f474beb6d77bf4e7b3c89d
nnnnusui <nnnnusui@gmail.com>
2019-12-15 16:41:16 +0900

    ♻️ TimelineCell

--- src/main/scala/com/github/nnnnusui/slideshow/Timeline.scala
+++ src/main/scala/com/github/nnnnusui/slideshow/Timeline.scala
@@ -1,7 +1,7 @@
 package com.github.nnnnusui
 package slideshow
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.Path
 
 import com.github.nnnnusui.slideshow.Timeline.Object.Picture
 import com.github.nnnnusui.slideshow.exo.TimelineObject
@@ -9,7 +9,7 @@ import com.github.nnnnusui.slideshow.exo.TimelineObject.Parameter
 import com.github.nnnnusui.slideshow.exo.filtereffect.StandardDrawing
 import javafx.scene.{input => jfxsi}
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.control.{ListCell, ListView, SelectionMode}
+import scalafx.scene.control.{ListView, SelectionMode}
 import scalafx.scene.input._
 
 import scala.jdk.CollectionConverters._
@@ -20,7 +20,7 @@ class Timeline {
   val view: ListView[Picture] = new ListView[Picture]{
     items.set(value)
     selectionModel.value.setSelectionMode(SelectionMode.Multiple)
-    cellFactory = _=> new TimelineCell
+    cellFactory = _=> new TimelineCell(this[Timeline])
     onDragOver    = event=>{ FilesDetector.onDragOver(new DragEvent(event));                event.consume() }
     onDragDropped = event=>{ FilesDetector.onDragDropped(new DragEvent(event), onDetect()); event.consume() }
     onKeyPressed = event=> {
@@ -32,66 +32,10 @@ class Timeline {
       }
     }
   }
-  private def onDetect(targetIndex: Int = 0): Seq[Path] => Unit = paths =>{
+  def onDetect(targetIndex: Int = 0): Seq[Path] => Unit = paths =>{
     val pictures = paths.map(it=> Picture(it.toAbsolutePath))
     value.addAll(targetIndex, pictures.asJava)
     valueLogger.logging(value.toVector)
-  }
-
-
-
-  class TimelineCell extends ListCell[Picture] {
-    item.onChange{ (_, _, value)=> text = if (value== null) "" else value.path.getFileName.toString }
-    onMouseClicked = event=> if (event.getButton == jfxsi.MouseButton.MIDDLE) remove()
-    onDragDetected = event=>{
-      LocalSorting.onDetect(new MouseEvent(event))
-      event.consume()
-    }
-    onDragOver = event=>{
-      LocalSorting.onDragOver(new DragEvent(event))
-      FilesDetector.onDragOver(new DragEvent(event))
-      event.consume()
-    }
-    onDragEntered = _=> listView.get().getSelectionModel.clearAndSelect(index.value)
-    onDragDropped = event=>{
-      LocalSorting.onDragDropped(new DragEvent(event))
-      FilesDetector.onDragDropped(new DragEvent(event), onDetect(Seq(value.size, index.value).min))
-      event.consume()
-    }
-    private def remove(): Unit = {
-      if(!empty.value)
-        value.remove(index.value)
-    }
-    object LocalSorting{
-      def onDetect(event: MouseEvent): Unit ={
-        if (item == null) return
-        val board = startDragAndDrop(TransferMode.Move)
-        val content = new ClipboardContent()
-        val items = listView.get().getSelectionModel.getSelectedItems.asScala
-                            .map(_.toString).toSeq
-        content.put(Timeline.dataFormat, items)
-        board.setContent(content)
-        /* remove */listView.get().getSelectionModel.getSelectedIndices.asScala.map(_.toInt).toSeq
-                            .reverse.foreach(index=> value.remove(index))
-      }
-      def onDragOver(event: DragEvent): Unit ={
-        if(!event.getDragboard.hasContent(Timeline.dataFormat)) return
-        event.acceptTransferModes(TransferMode.Move)
-      }
-      def onDragDropped(event: DragEvent): Unit ={
-        val board = event.getDragboard
-        if (!board.hasContent(Timeline.dataFormat)) return
-        val sources = board.getContent(Timeline.dataFormat).asInstanceOf[List[String]]
-                           .map(str=> Picture(Paths.get(str)))
-        val targetIndex = Seq(index.value, value.size).min
-        value.addAll(targetIndex, sources.asJava)
-        event.setDropCompleted(true)
-        /* select */ val selectionModel = listView.get().getSelectionModel
-          selectionModel.clearSelection()
-          selectionModel.selectRange(targetIndex, targetIndex + sources.size)
-        valueLogger.logging(value.toVector)
-      }
-    }
   }
 }
 
---
+++ src/main/scala/com/github/nnnnusui/slideshow/TimelineCell.scala
@@ -0,0 +1,64 @@
+package com.github.nnnnusui.slideshow
+
+import java.nio.file.Paths
+
+import com.github.nnnnusui.slideshow.Timeline.Object.Picture
+import javafx.scene.{input => jfxsi}
+import scalafx.scene.control.ListCell
+import scalafx.scene.input.{ClipboardContent, DragEvent, MouseEvent, TransferMode}
+
+import scala.jdk.CollectionConverters._
+
+class TimelineCell(val tl: Timeline) extends ListCell[Picture] {
+  item.onChange{ (_, _, value)=> text = if (value== null) "" else value.path.getFileName.toString }
+  onMouseClicked = event=> if (event.getButton == jfxsi.MouseButton.MIDDLE) remove()
+  onDragDetected = event=>{
+    LocalSorting.onDetect(new MouseEvent(event))
+    event.consume()
+  }
+  onDragOver = event=>{
+    LocalSorting.onDragOver(new DragEvent(event))
+    FilesDetector.onDragOver(new DragEvent(event))
+    event.consume()
+  }
+  onDragEntered = _=> listView.get().getSelectionModel.clearAndSelect(index.value)
+  onDragDropped = event=>{
+    LocalSorting.onDragDropped(new DragEvent(event))
+    FilesDetector.onDragDropped(new DragEvent(event), tl.onDetect(Seq(tl.value.size, index.value).min))
+    event.consume()
+  }
+  private def remove(): Unit = {
+    if(!empty.value)
+      tl.value.remove(index.value)
+  }
+  object LocalSorting{
+    def onDetect(event: MouseEvent): Unit ={
+      if (item == null) return
+      val board = startDragAndDrop(TransferMode.Move)
+      val content = new ClipboardContent()
+      val items = listView.get().getSelectionModel.getSelectedItems.asScala
+                          .map(_.toString).toSeq
+      content.put(Timeline.dataFormat, items)
+      board.setContent(content)
+      /* remove */listView.get().getSelectionModel.getSelectedIndices.asScala.map(_.toInt).toSeq
+                          .reverse.foreach(index=> tl.value.remove(index))
+    }
+    def onDragOver(event: DragEvent): Unit ={
+      if(!event.getDragboard.hasContent(Timeline.dataFormat)) return
+      event.acceptTransferModes(TransferMode.Move)
+    }
+    def onDragDropped(event: DragEvent): Unit ={
+      val board = event.getDragboard
+      if (!board.hasContent(Timeline.dataFormat)) return
+      val sources = board.getContent(Timeline.dataFormat).asInstanceOf[List[String]]
+                         .map(str=> Picture(Paths.get(str)))
+      val targetIndex = Seq(index.value, tl.value.size).min
+      tl.value.addAll(targetIndex, sources.asJava)
+      event.setDropCompleted(true)
+      /* select */ val selectionModel = listView.get().getSelectionModel
+        selectionModel.clearSelection()
+        selectionModel.selectRange(targetIndex, targetIndex + sources.size)
+      tl.valueLogger.logging(tl.value.toVector)
+    }
+  }
+}
